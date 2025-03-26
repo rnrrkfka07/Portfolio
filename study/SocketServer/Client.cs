221d36d70fa26abca24025df5bdbbbc061f4ba98
G4ram <65856313+rnrrkfka07@users.noreply.github.com>
2025-03-26 13:02:50 +0900

    Update Client.cs

--- study/SocketServer/Client.cs
+++ study/SocketServer/Client.cs
@@ -12,17 +12,19 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            //끝점 생성 (서버 IP 및 포트)
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.16.14.155"), 8211);
-
+            //소켓 생성
             Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
+            //소켓을 끝점에 연결
             socket.Connect(endPoint);
             Console.WriteLine("Connect...");
 
             while (true) { 
                 String str = Console.ReadLine();
                 byte[] sendBuff = Encoding.UTF8.GetBytes(str);
-                socket.Send(sendBuff);
+                socket.Send(sendBuff); //입력받은 문자열을 서버에 전송
                 if(str == "exit")
                 {
                     break;
