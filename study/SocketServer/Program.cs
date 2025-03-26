c44ac9b649204a458ae150141f9007ae3484b53b
G4ram <65856313+rnrrkfka07@users.noreply.github.com>
2025-03-26 12:03:41 +0900

    Add files via upload

---
+++ study/SocketServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.16.14.155"), 8211);
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            listenSocket.Bind(endPoint);
+            listenSocket.Listen(10);
+
+            
+            Socket clientSocket = listenSocket.Accept();
+            byte[] recvBuff = new byte[1024];
+            while (true)
+            {
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                if(recvData == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine($"Client : {recvData}");
+            }
+            listenSocket.Close();
+        }
+    }
+}
