42a8d97c2b6a85380b801ee143a974c72af9dbc0
G4ram <65856313+rnrrkfka07@users.noreply.github.com>
2025-03-26 13:01:03 +0900

    Add files via upload

---
+++ study/SocketServer/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.16.14.155"), 8211);
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endPoint);
+            Console.WriteLine("Connect...");
+
+            while (true) { 
+                String str = Console.ReadLine();
+                byte[] sendBuff = Encoding.UTF8.GetBytes(str);
+                socket.Send(sendBuff);
+                if(str == "exit")
+                {
+                    break;
+
+                }
+            }
+            socket.Close();
+        }
+    }
+}
